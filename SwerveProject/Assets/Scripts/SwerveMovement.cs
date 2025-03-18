e840bfa64be416bb2ea1c95d62193743b74d72ce
sai <saahilfardhook@gmail.com>
2025-03-18 13:34:29 -0700

    fixed

--- SwerveProject/Assets/Scripts/SwerveMovement.cs
+++ SwerveProject/Assets/Scripts/SwerveMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine.InputSystem;
 public class SwerveMovement : MonoBehaviour
 {
     Vector2 moveDirection = Vector2.zero;
-    Vector2 rotationDirection = Vector2.zero;   
+    float rotationDirection;   
     [SerializeField] InputAction playerControls;
+    [SerializeField] private InputAction moveAction;
+    [SerializeField] private InputAction rotateAction;
+    [SerializeField] private InputAction slowAction;
     private float moveSpeed = 4.75f;
+    private float slowSpeed = 1;
     private Rigidbody rb;
 
     private void Awake()
@@ -17,25 +21,38 @@ public class SwerveMovement : MonoBehaviour
     private void OnEnable()
     {
         playerControls.Enable();
+        rotateAction.Enable();
+        slowAction.Enable();
+        slowAction.performed += Slow;
     }
 
     private void OnDisable()
     {
         playerControls.Disable();
+        rotateAction.Disable();
+        slowAction.Disable();
+        slowAction.performed -= Slow;
+    }
+
+    private void Slow(InputAction.CallbackContext context)
+    {
+        rb.velocity = new Vector3(moveDirection.y * -slowSpeed, 0, moveDirection.x * slowSpeed);
+        rb.angularVelocity = new Vector3(0, rotationDirection * 30F, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         moveDirection = playerControls.ReadValue<Vector2>();
-        rotationDirection = playerControls.ReadValue<Vector2>();
+        rotationDirection = rotateAction.ReadValue<Vector2>().x;
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(moveDirection.y * -moveSpeed, 0, moveDirection.x * moveSpeed);
-        rb.angularVelocity = new Vector2(0, rotationDirection.x * 1.5F);
-        Debug.Log($"Lateral: {moveDirection.x}, Longitudal: {moveDirection.y}");
+        rb.angularVelocity = new Vector3(0, rotationDirection * 30F, 0);
+        Debug.Log($"Lateral: {moveDirection.x}, Longitudal: {moveDirection.y}, Rotational: {rotationDirection}");
+        Debug.Log(rb.angularVelocity);
     }
 
 
